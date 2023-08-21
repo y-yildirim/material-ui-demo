f599fece6025d6272d3827539c1d4ec32bd43cf7
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2023-08-21 10:51:13 +0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
-import { Fragment } from "react";
-import { Container, Grid, Typography } from "@mui/material";
-
-import TourCard from "./components/TourCard";
-import cities from "./data.json";
-
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Container>
-        {cities.map((city) => (
-          <Fragment key={city.id}>
-            <Typography
-              variant="h4"
-              component={"h2"}
-              marginTop={5}
-              marginBottom={3}
-            >
-              Top {city.name} Tours
-            </Typography>
-            <Grid container spacing={1}>
-              {city.tours.map((tour, index) => (
-                <TourCard key={index} tour={tour} />
-              ))}
-            </Grid>
-          </Fragment>
-        ))}
-      </Container>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import { Fragment } from "react";
+import { Container, Grid, Typography } from "@mui/material";
+
+import TourCard from "../components/TourCard";
+import cities from "../data.json";
+
+function Home() {
+  return (
+    <div className="App">
+      <Container>
+        {cities.map((city) => (
+          <Fragment key={city.id}>
+            <Typography
+              variant="h4"
+              component={"h2"}
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={1}>
+              {city.tours.map((tour, index) => (
+                <TourCard key={index} tour={tour} />
+              ))}
+            </Grid>
+          </Fragment>
+        ))}
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
